5f800ad366a983ba192830f01dde2cd3755df35f
ryurujxx <ryuruJ43@users.noreply.github.com>
2026-01-08 11:43:46 +0900

    style: Movie 및 SeatType 표준 스타일로 재수정

--- backend/src/main/java/com/cinema/kino/entity/enums/SeatType.java
+++ backend/src/main/java/com/cinema/kino/entity/enums/SeatType.java
@@ -1,8 +1,10 @@
 package com.cinema.kino.entity.enums;
 
 public enum SeatType {
-    NORMAL,     // 일반석
-    VIP,        // VIP석
-    DISABLED,   // 장애인석
-    COUPLE      // 커플석
+    NORMAL,       // 일반석
+    DISABLED,     // 장애인석
+    COUPLE,       // 커플석
+    ENTRANCE,     // 입구
+    EXIT,         // 출구
+    ENTRANCE_EXIT // 입출구 통합
 }
